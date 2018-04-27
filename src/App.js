0aa9f5be927fd2ddd935de6292b78b37e2cae899
Meron Kebede <kebedemeron905@gmail.com>
2018-04-27 15:39:09 -0400

    got url to show up on page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react'
+
+import { Route, Link } from 'react-router-dom'
+import Books from './Books.js'
 import axios from 'axios'
 import './App.css'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+
+      books: []
+
+    }
+  }
+  componentDidMount () {
+    axios.get('http://localhost:4004/')
+      .then(response => {
+        const books = response.data
+        this.setState({ books })
+      })
+  }
+
   render () {
     return (
       <div>
         <h1> BookshelV</h1>
-        {/* <nav>
-          <Link to="/book"></Link>
+        <nav>
+
+          { this.state.books.map(book => <li>{book.url}</li>) }
+          {/* <Link to='/book'>BookshelV</Link> */}
         </nav>
         <main>
-          <Route path="" render={}/>
-        </main> */}
+
+          {/* <Route path="" render={}/> */}
+        </main>
       </div>
     )
   }
